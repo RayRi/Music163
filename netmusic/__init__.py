bedc20b9bbed067236174552d94eff9d21b36dce
ZenRay <jake2011ren@gmail.com>
2021-03-09 16:36:13 +0800

    feat($playlist): Add Playlist Request
    添加播放列表请求

---
+++ netmusic/__init__.py
@@ -0,0 +1,31 @@
+#coding:utf8
+
+from .utils.decrypt import get_params
+from .utils.base import Request
+
+class PlayList(Request):
+    """
+    获取收藏列表
+
+    通过列表 ID，获取收藏列表中的歌单信息
+    """
+    def __init__(self) -> None:
+        self.endpoint = "/weapi/v6/playlist/detail"
+
+    def request(self, id, **kwargs):
+        # 请求需要的表单数据
+        item = {
+            "id":str(id), 
+            "offset": "0", 
+            "total":True, 
+            "limit":"1000",
+            "n":"1000"
+        }
+        # 请求参数
+        params = {"csrf_token": ""}
+
+        # 获取请求体参数
+        item.update(params)
+        data = get_params(item)
+
+        response = super().request(self.endpoint, "POST", params=params, data=data)
