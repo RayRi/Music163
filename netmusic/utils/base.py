ff33fa82336f8b9dbd258ac370eca26447b85d86
ZenRay <jake2011ren@gmail.com>
2021-03-09 16:35:16 +0800

    获取请求的基类

---
+++ netmusic/utils/base.py
@@ -0,0 +1,26 @@
+#coding:utf8
+import urllib
+import requests
+
+class Request:
+    """
+    网络请求
+    """
+    HOST = "https://music.163.com"
+    def __init__(self) -> None:
+        pass
+    
+    def request(self, url, method="POST", **kwargs):
+        """发出网络请求"""
+        if "http" not in url:
+            url = urllib.parse.urljoin(self.HOST, url)
+        
+        response = requests.Request(method, url, kwargs)
+
+        if response.status_code != 200:
+            raise requests.RequestException(f"请求异常，状态码为: {response.status_code}")
+            
+        if len(response.text) == 0:
+            raise Exception("响应内容异常")
+
+        return response
